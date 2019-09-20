acfb5568adb3245254ef398fe8b49e2b79c32fc0
Frederic Marion-Poll <marion_poll@hotmail.com>
2019-09-20 19:01:12 +0200

    minor change

--- src/plugins/fmp/multicafe/MultiCAFE.java
+++ src/plugins/fmp/multicafe/MultiCAFE.java
@@ -24,7 +24,7 @@ import plugins.fmp.multicafeSequence.SequenceCamData;
 
 // SequenceListener?
 public class MultiCAFE extends PluginActionable implements ViewerListener, PropertyChangeListener {
-	IcyFrame 					mainFrame 			= new IcyFrame("MultiCAFE analysis 18-Sept-2019", true, true, true, true);
+	IcyFrame 					mainFrame 			= new IcyFrame("MultiCAFE analysis 19-Sept-2019", true, true, true, true);
 	ExperimentList				expList 			= new ExperimentList();
 	int							currentIndex		= -1;
 	
