425825e4b3b7594d64ea1ee07d7ac1801e4d4591
fred <fred@TASTE-LAPTOP-FRED>
2019-12-28 16:33:40 +0100

    misc: change date on multiCAFE dialog box

--- src/plugins/fmp/multicafe/MultiCAFE.java
+++ src/plugins/fmp/multicafe/MultiCAFE.java
@@ -24,7 +24,7 @@ import plugins.fmp.multicafeSequence.SequenceCamData;
 
 // SequenceListener?
 public class MultiCAFE extends PluginActionable implements ViewerListener, PropertyChangeListener {
-	IcyFrame 		mainFrame 		= new IcyFrame("MultiCAFE analysis 07-Dec-2019", true, true, true, true);
+	IcyFrame 		mainFrame 		= new IcyFrame("MultiCAFE analysis 28-Dec-2019", true, true, true, true);
 	ExperimentList	expList 		= new ExperimentList();
 	int				currentExperimentIndex	= -1;
 	
