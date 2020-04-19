45441de4d695ae685eb71f0118f6ee0824204c8d
fred <fred@TASTE-LAPTOP-FRED>
2020-04-19 21:49:51 +0200

    update to capillary table model

--- src/plugins/fmp/multicafeSequence/CapillaryTableModel.java
+++ src/plugins/fmp/multicafeSequence/CapillaryTableModel.java
@@ -2,8 +2,20 @@ package plugins.fmp.multicafeSequence;
 
 import javax.swing.table.AbstractTableModel;
 
-public class CapillaryTableModel extends AbstractTableModel  {
+import plugins.fmp.multicafe.MultiCAFE;
 
+public class CapillaryTableModel extends AbstractTableModel  {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6325792669154093747L;
+	private MultiCAFE parent0 = null;
+	
+	public CapillaryTableModel (MultiCAFE parent0) {
+		super();
+		this.parent0 = parent0;
+	}
+	
 	@Override
 	public int getColumnCount() {
 		// TODO Auto-generated method stub
