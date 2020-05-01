9ee42c847715f1771b2d0d651884bb666bd23ffb
Frederic Marion-Poll <marion_poll@hotmail.com>
2020-05-01 14:06:36 +0200

    fix bug / save gulps detection

--- src/plugins/fmp/multicafeTools/DetectGulps_series.java
+++ src/plugins/fmp/multicafeTools/DetectGulps_series.java
@@ -75,7 +75,9 @@ public class DetectGulps_series extends SwingWorker<Integer, Integer> {
 	
 	private void saveComputation(Experiment exp) {			
 		ProgressFrame progress = new ProgressFrame("Save kymograph measures");		
-		exp.saveExperimentMeasures();
+		//exp.saveExperimentMeasures();
+		exp.xmlSaveMCcapillaries();
+		exp.xmlSaveKymos_Measures();
 		progress.close();
 	}
 	
