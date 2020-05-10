ea680d4089f34d00cff65a675b5de4d1d8312286
Frederic Marion-Poll <marion_poll@hotmail.com>
2020-05-10 23:45:48 +0200

    minor change to dialog vgap && date

--- src/plugins/fmp/multicafe/MCCapillaries_Infos.java
+++ src/plugins/fmp/multicafe/MCCapillaries_Infos.java
@@ -46,8 +46,7 @@ public class MCCapillaries_Infos extends JPanel {
 		setLayout(capLayout);
 		this.parent0 = parent0;
 		
-		JPanel panel0 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		((FlowLayout)panel0.getLayout()).setVgap(0);
+		JPanel panel0 = new JPanel(new FlowLayout(FlowLayout.LEFT, 3, 1));
 		panel0.add( new JLabel("volume (µl) ", SwingConstants.RIGHT));
 		panel0.add( capillaryVolumeTextField);
 		panel0.add( new JLabel("length (pixels) ", SwingConstants.RIGHT));
