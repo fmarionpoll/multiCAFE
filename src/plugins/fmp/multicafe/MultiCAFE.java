fc619f76acbfbbbdb01c1f880e6b41229a4d87e1
Frederic Marion-Poll <marion_poll@hotmail.com>
2020-08-23 18:24:19 +0200

    change date

--- src/plugins/fmp/multicafe/MultiCAFE.java
+++ src/plugins/fmp/multicafe/MultiCAFE.java
@@ -29,7 +29,7 @@ import plugins.fmp.multicafeSequence.ExperimentList;
 
 
 public class MultiCAFE extends PluginActionable implements ViewerListener, PropertyChangeListener {
-	public IcyFrame 		mainFrame 		= new IcyFrame("MultiCAFE 20-August-2020", true, true, true, true);
+	public IcyFrame 		mainFrame 		= new IcyFrame("MultiCAFE 23-August-2020", true, true, true, true);
 	public ExperimentList 	expList 	= new ExperimentList();
 	
 	public MCSequence_ 		paneSequence 	= new MCSequence_();
